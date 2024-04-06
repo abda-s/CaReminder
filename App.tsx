1c363c8827fbfb114bfb1169db80feddad2c7f9e
abdullah salameh <abdullahsalameh66@gmail.com>
2024-04-06 20:42:34 +0300

    initilaization of the project

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+
+} from 'react-native';
+
+
+function App(){
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eos rerum ipsa. Tempora dolorum magni similique corrupti quo, beatae minima qui enim soluta tempore omnis fugiat fuga animi ipsum temporibus. </Text>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
